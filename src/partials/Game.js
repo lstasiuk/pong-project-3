2629713f079ad6315d1b54e84430fad950100180
lstasiuk <leanne.stasiuk@gmail.com>
2018-08-03 09:24:45 -0700

    set height and width for paddles to go

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,15 +1,59 @@
+import { SVG_NS, KEYS } from '../settings';
+import Board from './Board';
+import Paddle from './Paddle';
+
 export default class Game {
 
-	constructor(element, width, height) {
-		this.element = element;
-		this.width = width;
-		this.height = height;
-	
-		// Other code goes here...
-	}
+    constructor(element, width, height) {
+        this.element = element;
+        this.width = width;
+        this.height = height;
+        this.gameElement = document.getElementById(this.element);
 
-	render() {
-		// More code goes here...
-	}
+        // make a new board with a new game
+        this.board = new Board(this.width, this.height);
+
+        // making players
+        this.paddleWidth = 8;
+        this.paddleHeight = 56;
+        this.boardGap = 10;
+        this.player1 = new Paddle(
+            this.height,
+            this.paddleWidth,
+            this.paddleHeight,
+            this.boardGap,
+            ((this.height - this.paddleHeight) / 2),
+            KEYS.a,
+            KEYS.z,
+        );
+
+
+        this.player2 = new Paddle(
+            this.height,
+            this.paddleWidth,
+            this.paddleHeight,
+            (this.width - this.boardGap - this.paddleWidth),
+            ((this.height - this.paddleHeight) / 2),
+            KEYS.up,
+            KEYS.down,
+        );
+        // Other code goes here...
+    }
+
+    render() {
+        this.gameElement.innerHTML = '';
+
+        let svg = document.createElementNS(SVG_NS, "svg");
+        svg.setAttributeNS(null, "width", this.width);
+        svg.setAttributeNS(null, "height", this.height);
+        svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
+        this.gameElement.appendChild(svg);
+        this.board.render(svg);
+
+        // render the board
+        this.board.render(svg);
+        this.player1.render(svg);
+        this.player2.render(svg);
+    }
 
 }
